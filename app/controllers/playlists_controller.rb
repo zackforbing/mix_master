9247eac37725f87c60533ad089cf735af4f49233
Zack Forbing <zackforbing@gmail.com>
2016-07-16 22:01:29 -0600

    i4-refactoring complete

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,5 @@
 class PlaylistsController < ApplicationController
+    before_action :set_playlist, only: [:show, :edit, :update,]
 
   def index
     @playlists = Playlist.all
@@ -14,15 +15,12 @@ class PlaylistsController < ApplicationController
   end
 
   def show
-    @playlist = Playlist.find(params[:id])
   end
 
   def edit
-    @playlist = Playlist.find(params[:id])
   end
 
   def update
-    @playlist = Playlist.find(params[:id])
     if @playlist.update(playlist_params)
       redirect_to @playlist
     else
@@ -31,6 +29,10 @@ class PlaylistsController < ApplicationController
   end
 
   private
+
+  def set_playlist
+    @playlist = Playlist.find(params[:id])
+  end
 
   def playlist_params
     params.require(:playlist).permit(:name, song_ids: [])
